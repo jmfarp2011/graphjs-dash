77d06a1940dac29081222d7ca16dd6d4d6c8401c
jmfarp2011 <jamesmeador1983@gmail.com>
2014-09-04 17:36:44 +0000

    added bootstrap panel

--- LayoutWidget.js
+++ LayoutWidget.js
@@ -1,22 +1,36 @@
 function Dashboard(_containerID){
     if (!$ || !jQuery) return false;
+    var $container = $('#'+_containerID);
+    $container.addClass('container');
     var _widgets = [],
     _dataObject = {};
+    
     this.bind = function(dataObject){
         _dataObject = dataObject;
     }
+    
+    this.layout = function(layout){
+        for (var i = 0; i < layout.rows.length; i++){
+            var $currentRow = $('<div>', {class:'row'});
+            $currentRow.appendTo($container);
+            for (var j = 0; j < layout.rows[i].cols.length; j++) {
+                var span = 'col-xs-12 col-sm-'+layout.rows[i].cols[j].span+' col-md-'+layout.rows[i].cols[j].span;
+                $currentRow.append($('<div>', {
+                    id: layout.rows[i].cols[j].widget, 
+                    class: span
+                }));
+            }
+        }
+    }
+    
     this.register = function(widget){
         //add widget to the widget stack
         _widgets.push(widget);
         
-        //create widget container div $widget
-        var $widget = $('<div>', { id: widget.id });
-        $widget.addClass('span'+widget.span)
-            .appendTo($('#'+_containerID));
-        
         //init widget
         widget.init(_dataObject);
     }
+    
     this.update = function(dataObject){
         _dataObject = dataObject;
         for (var i = 0; i < _widgets.length; i++){
